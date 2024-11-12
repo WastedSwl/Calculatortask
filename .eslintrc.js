53d6e625fcc788872ccf55f4addb52ba50618f83
wastedswl <siryvlad@gmail.com>
2024-11-13 00:25:54 +0300

    Test pre-commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js'; // Импортируем базовые правила ESLint
+import prettier from 'eslint-plugin-prettier'; // Импортируем плагин prettier
+
+export default {
+  files: ['src/*.js'], // Применяется только к файлам в папке src
+  languageOptions: {
+    ecmaVersion: 2021, // Используем ES2021
+    sourceType: 'module', // Указываем, что используем модули
+    globals: {
+      document: 'readonly', // Для поддержки переменных браузера
+      window: 'readonly',
+    },
+  },
+  plugins: {
+    prettier, // Указываем плагин как объект
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended', // Включаем конфигурацию Prettier
+  ],
+  rules: {
+    'prettier/prettier': 'error', // Ошибки по правилам prettier
+    'no-unused-vars': 'error', // Ошибки на неиспользуемые переменные
+    'no-console': 'error', // Ошибки на использование console
+  },
+};
